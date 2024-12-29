57ea97f08068e32ec477c4bdb254478f42727661
josueprlt <148058543+josueprlt@users.noreply.github.com>
2024-12-29 16:31:40 +0100

    update

--- app/project/[id]/page.tsx
+++ app/project/[id]/page.tsx
@@ -1,9 +1,21 @@
+"use client"
+
+import { useEffect, useState } from "react";
+import { useRouter, useSearchParams } from "next/navigation";
 import Home from '@/app/components/ui/projectPage/home/page';
 import Tools from "@/app/components/ui/projectPage/tools/page";
 import Description from "@/app/components/ui/projectPage/description/page";
 import Images from "@/app/components/ui/projectPage/images/page";
 
 export default function Page() {
+    const searchParams = useSearchParams();
+
+    useEffect(() => {
+
+        console.log(searchParams);
+
+    }, [searchParams]);
+
     return (
         <>
             <Home />
