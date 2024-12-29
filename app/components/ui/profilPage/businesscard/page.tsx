5d831ff5b3ac9b833faec9b3f063ab9b77cad522
josueprlt <148058543+josueprlt@users.noreply.github.com>
2024-12-29 12:39:19 +0100

    up

--- app/components/ui/profilPage/businesscard/page.tsx
+++ app/components/ui/profilPage/businesscard/page.tsx
@@ -1,18 +1,35 @@
-import { Dela_Gothic_One } from 'next/font/google';
+import { Climate_Crisis } from 'next/font/google';
+import { PhoneIcon, EmailIcon, LinkIcon, LocationIcon } from '@/app/components/ui/icons';
+import Image from 'next/image';
 
-const DelaGothicOne = Dela_Gothic_One({
+const ClimateCrisis = Climate_Crisis({
     subsets: ['latin'],
-    weight: ['400'],
     display: 'swap',
 });
 
 export default function BusinessCard() {
     return (
-        <div className={`${DelaGothicOne.className} pt-20 md:pt-60`}>
+        <div className={`${ClimateCrisis.className} pt-20 md:pt-60`}>
 
-            <div className="flex flex-col gap-4">
+            <section className="max-w-96 mx-auto flex items-center flex-col gap-10 pt-10 bg-foreground rounded-lg shadow-lg overflow-hidden">
+                <div className='flex items-center flex-col gap-2'>
+                    <p className='text-2xl text-background'>Perrault Josué</p>
+                    <p className='text-xl text-secondary'>Développeur Web</p>
+                </div>
+
+                <div className='w-48 h-48 flex justify-center items-center bg-background rounded-full overflow-hidden'>
+                    <Image src="/img/profil.png" alt="Image de profil" width={182} height={245} />
+                </div>
+
+                <div className='flex items-center flex-col gap-2'>
+                    <PhoneIcon className='w-6 h-6 text-primary' />
+                    <EmailIcon className='w-6 h-6 text-primary' />
+                    <LinkIcon className='w-6 h-6' />
+                    <LocationIcon className='w-6 h-6' />
+                </div>
                 
-            </div>
+                <span className='block pt-10 w-full h-5 bg-gradient-to-r from-primary to-secondary'></span>
+            </section>
         </div>
     );
 }
