2930a2d218c0c08a61d241b33454a057a201960b
josueprlt <148058543+josueprlt@users.noreply.github.com>
2024-12-29 21:10:01 +0100

    Update Tools project

--- app/components/ui/projectPage/tools/page.tsx
+++ app/components/ui/projectPage/tools/page.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from "react";
 import { Climate_Crisis, Dela_Gothic_One } from 'next/font/google';
-import { HtmlIcon, MuiIcon, ReactIcon } from '@/app/components/ui/icons';
+import { HtmlIcon, CssIcon, JsIcon, SassIcon, PhpIcon, ReactIcon, SymfonyIcon, BootstrapIcon, MuiIcon, TailwindIcon, NextIcon } from '@/app/components/ui/icons';
 
 const DelaGothicOne = Dela_Gothic_One({
     subsets: ['latin'],
@@ -29,7 +29,7 @@ interface HomeProps {
     project: Project;
 }
 
-const technos = ["html", "css", "js", "react", "sass", "mui"];
+const technos = ["html", "css", "js", "sass", "php", "react", "symfony", "bootstrap", "mui", "tailwind", "next"];
 
 export default function Tools({ project }: HomeProps) {
     const [projet, setProjet] = useState<Project | null>(null);
@@ -54,9 +54,17 @@ export default function Tools({ project }: HomeProps) {
             <section className={`${DelaGothicOne.className} mt-5 flex flex-wrap gap-4 md:mt-10`}>
                 {filteredTechnos.map((tech, index) => (
                     <div key={index} className='w-max flex items-center gap-2 outline p-2 md:p-4 rounded-lg'>
-                        {tech === "html" && (<><HtmlIcon className='w-8 h-8' /> <span className="text-">{tech}</span></>)}
-                        {tech === "mui" && (<><MuiIcon className='w-8 h-8' /> <span>{tech}</span></>)}
-                        {tech === "react" && (<><ReactIcon className='w-8 h-8' /> <span>{tech}</span></>)}
+                        {tech === "html" && (<><HtmlIcon className='w-8 h-8' /> <span className="text-html">{tech}</span></>)}
+                        {tech === "css" && (<><CssIcon className='w-8 h-8' /> <span className="text-css">{tech}</span></>)}
+                        {tech === "js" && (<><JsIcon className='w-8 h-8' /> <span className="text-js">{tech}</span></>)}
+                        {tech === "sass" && (<><SassIcon className='w-8 h-8' /> <span className="text-sass">{tech}</span></>)}
+                        {tech === "php" && (<><PhpIcon className='w-8 h-8' /> <span className="text-php">{tech}</span></>)}
+                        {tech === "react" && (<><ReactIcon className='w-8 h-8' /> <span className="text-react">{tech}</span></>)}
+                        {tech === "symfony" && (<><SymfonyIcon className='w-8 h-8' /> <span className="text-symfony">{tech}</span></>)}
+                        {tech === "bootstrap" && (<><BootstrapIcon className='w-8 h-8' /> <span className="text-bootstrap">{tech}</span></>)}
+                        {tech === "mui" && (<><MuiIcon className='w-8 h-8' /> <span className="text-materialui">{tech}</span></>)}
+                        {tech === "tailwind" && (<><TailwindIcon className='w-8 h-8' /> <span className="text-tailwind">{tech}</span></>)}
+                        {tech === "next" && (<><NextIcon className='w-8 h-8' /> <span className="text-symfony">{tech}</span></>)}
                     </div>
                 ))}
             </section>
