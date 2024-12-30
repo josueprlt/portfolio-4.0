d4897bc6423693981000ee64ff3a80b281f8786d
josueprlt <148058543+josueprlt@users.noreply.github.com>
2024-12-30 12:34:20 +0100

    update

--- app/components/ui/homePage/home/page.tsx
+++ app/components/ui/homePage/home/page.tsx
@@ -1,6 +1,10 @@
+"use client"
+
+import { useEffect } from "react";
 import { ArrowIcon } from "@/app/components/ui/icons";
 import NavBar from "@/app/components/ui/navBar/page";
 import { Climate_Crisis } from 'next/font/google';
+import { gsap } from "gsap";
 
 const ClimateCrisis = Climate_Crisis({
     subsets: ['latin'],
@@ -8,13 +12,29 @@ const ClimateCrisis = Climate_Crisis({
 });
 
 export default function Home() {
+    useEffect(() => {
+        // Animation pour déplacer les deux spans du h1
+        gsap.fromTo(
+            ".animate-span",
+            { x: -100, opacity: 0 },
+            { x: 0, opacity: 1, duration: 1, stagger: 0.2 }
+        );
+
+        // Animation pour faire apparaître le span caché du h1
+        gsap.to(".hidden-span", { opacity: 1, duration: 1, delay: 1 });
+    }, []);
+
     return (
         <header className={`${ClimateCrisis.className} flex flex-col justify-between h-screen p-4 md:p-8`}>
             <NavBar />
             
             <section className="flex flex-col justify-center items-center gap-16">
                 <div className="flex items-center flex-col gap-7">
-                    <h1 className="text-3xl text-center md:text-7xl">Josué Perrault</h1>
+                    <h1 className="text-3xl flex justify-center items-center flex-wrap gap-4 text-center md:gap-8 md:text-7xl">
+                        <span>Josué</span>
+                        <span className="hidden w-24 h-24 rounded-full bg-gradient-to-b from-primary to-secondary"></span>
+                        <span>Perrault</span>
+                    </h1>
                     <span className="w-52 h-1.5 bg-gradient-to-r from-primary to-secondary rounded-full md:w-96 md:h-3"></span>
                 </div>
                 <ArrowIcon className="cursor-pointer" />
