f3234b107c5734853cbf3b756e465377a69723ae
josueprlt <148058543+josueprlt@users.noreply.github.com>
2025-01-19 19:10:39 +0100

    update

--- app/components/ui/projectPage/home/page.tsx
+++ app/components/ui/projectPage/home/page.tsx
@@ -1,20 +1,21 @@
-"use client"
+"use client";
 
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import NavBar from "@/app/components/ui/navBar/page";
-import { Climate_Crisis, Dela_Gothic_One } from 'next/font/google';
-import Image from 'next/image';
+import { Climate_Crisis, Dela_Gothic_One } from "next/font/google";
+import Image from "next/image";
 import { Tooltip } from "@nextui-org/tooltip";
+import { gsap, Power2 } from "gsap";
 
 const DelaGothicOne = Dela_Gothic_One({
-    subsets: ['latin'],
-    weight: ['400'],
-    display: 'swap',
+    subsets: ["latin"],
+    weight: ["400"],
+    display: "swap",
 });
 
 const ClimateCrisis = Climate_Crisis({
-    subsets: ['latin'],
-    display: 'swap',
+    subsets: ["latin"],
+    display: "swap",
 });
 
 interface Project {
@@ -28,43 +29,155 @@ interface Project {
 }
 
 interface HomeProps {
-    project: Project;
+    project: Project | null; // Permettre que project soit null
 }
 
 export default function Home({ project }: HomeProps) {
-    const [projet, setProjet] = useState<Project | null>(null);
-    const [numberOfItem, setNumberOfItem] = useState<number>(2);
+    const numberOfItem = 2;
+    const sectionRef = useRef<HTMLDivElement>(null);
+    const h1Ref = useRef<HTMLHeadingElement>(null);
+    const spanRefs = useRef<HTMLSpanElement[]>([]);
+    const spanRef2 = useRef<HTMLSpanElement>(null);
+    const imageRef = useRef<HTMLImageElement>(null);
+
+    const [animationsPlayed, setAnimationsPlayed] = useState(() => {
+        if (typeof window !== 'undefined') {
+            return localStorage.getItem('animationsPlayedHome') === 'true';
+        }
+        return false;
+    });
 
     useEffect(() => {
-        setProjet(project);
-    }, [project]);
+        const handleBeforeUnload = () => {
+            localStorage.removeItem('animationsPlayedHome');
+        };
 
-    if (!projet) {
+        window.addEventListener('beforeunload', handleBeforeUnload);
+
+        return () => {
+            window.removeEventListener('beforeunload', handleBeforeUnload);
+        };
+    }, []);
+
+    useEffect(() => {
+        if (animationsPlayed) return;
+
+        if (document.fonts) {
+            document.fonts.ready.then(() => {
+                const spanElements = spanRefs.current;
+                const spanElement2 = spanRef2.current;
+                const h1Element = h1Ref.current;
+                const imgElement = imageRef.current;
+
+                if (spanElements.length > 0) {
+                    gsap.from(spanElements, {
+                        delay: .5,
+                        opacity: 0,
+                        x: 50,
+                        duration: 1,
+                        ease: Power2.easeOut,
+                        stagger: 0.5,
+                    });
+                }
+
+                if (spanElement2) {
+                    gsap.from(spanElement2, {
+                        delay: 1.5,
+                        opacity: 0,
+                        x: 50,
+                        duration: 1,
+                        ease: Power2.easeOut,
+                    });
+                }
+
+                if (h1Element) {
+                    gsap.from(h1Element, {
+                        opacity: 0,
+                        x: -50,
+                        duration: 1,
+                        ease: Power2.easeOut,
+                    });
+                }
+
+                if (imgElement) {
+                    gsap.from(imgElement, {
+                        delay: 2,
+                        filter: "grayscale(100%) blur(5px)",
+                        duration: 1,
+                        ease: Power2.easeOut,
+                    });
+                }
+            });
+        }
+
+        setAnimationsPlayed(true);
+        localStorage.setItem('animationsPlayedHome', 'true');
+    }, [animationsPlayed]);
+
+    // Afficher un état de chargement si project est null
+    if (!project) {
         return <div>Loading...</div>;
     }
 
-    const remainingCount = projet.category.length - numberOfItem;
+    const remainingCount = project.category.length - numberOfItem;
 
     return (
         <header className={`${ClimateCrisis.className} flex flex-col p-4 md:p-8`}>
             <NavBar />
 
-            <section className="h-96 flex justify-center items-center relative overflow-hidden mt-20 rounded-2xl after:absolute after:bottom-0 after:left-0 after:w-full after:h-full after:bg-gradient-to-t after:from-foreground after:to-transparent after:z-5 md:justify-start md:items-end md:px-10 md:pb-8 md:after:h-48">
-
+            <section
+                ref={sectionRef}
+                className="h-96 flex justify-center items-center relative overflow-hidden mt-20 rounded-2xl after:absolute after:bottom-0 after:left-0 after:w-full after:h-full after:bg-gradient-to-t after:from-foreground after:to-transparent after:z-5 md:justify-start md:items-end md:px-10 md:pb-8 md:after:h-full"
+            >
                 <div className="md:flex md:flex-row md:justify-between md:items-end md:w-full">
-                    <h1 className="relative z-10 text-background text-4xl text-center md:text-7xl md:text-left">{projet.title}</h1>
-                    <div className={`${DelaGothicOne.className} absolute flex flex-wrap-reverse gap-2 bottom-0 left-0 z-10 p-2 text-xs md:relative md:w-48 md:justify-end`}>
-                        {projet.category.slice(0, numberOfItem).map((cat, index) => (
-                            <span key={index} className="bg-background text-foreground px-4 py-2 rounded-full">{cat}</span>
+                    <h1
+                        ref={h1Ref}
+                        className="relative z-10 text-background text-4xl text-center md:text-7xl md:text-left"
+                    >
+                        {project.title}
+                    </h1>
+
+                    <div
+                        className={`${DelaGothicOne.className} absolute flex flex-wrap-reverse gap-2 bottom-0 left-0 z-10 p-2 text-xs md:relative md:w-48 md:justify-end`}
+                    >
+                        {project.category.slice(0, numberOfItem).map((cat, index) => (
+                            <span
+                                ref={(el) => {
+                                    if (el) spanRefs.current[index] = el;
+                                }}
+                                key={index}
+                                className="bg-background text-foreground px-4 py-2 rounded-full"
+                            >
+                                {cat}
+                            </span>
                         ))}
                         {remainingCount > 0 && (
-                            <Tooltip content={projet.category.slice(numberOfItem).join(', ')} className={`${DelaGothicOne.className} bg-background color-foreground text-xs`} showArrow={true}>
-                                <span className="bg-background text-foreground px-4 py-2 rounded-full">+ {remainingCount}</span>
-                            </Tooltip>
+                            <div ref={spanRef2} className="bg-background text-foreground px-4 py-2 rounded-full">
+                                <Tooltip
+                                    content={project.category.slice(numberOfItem).join(", ")}
+                                    className={`${DelaGothicOne.className} bg-background color-foreground text-xs`}
+                                    showArrow={true}
+                                >
+                                    <span
+                                        className=""
+                                    >
+                                        + {remainingCount}
+                                    </span>
+                                </Tooltip>
+                            </div>
                         )}
+
                     </div>
                 </div>
-                <Image width={5000} height={5000} src={project.image[0]} alt="Description of the image" className="w-full h-full absolute top-0 left-0 z-0 object-cover" />
+
+                <Image
+                    ref={imageRef}
+                    width={5000}
+                    height={5000}
+                    src={project.image[0]}
+                    alt="Description of the image"
+                    className="w-full h-full absolute top-0 left-0 z-0 object-cover"
+                />
             </section>
         </header>
     );
--- app/components/ui/projectPage/tools/page.tsx
+++ app/components/ui/projectPage/tools/page.tsx
@@ -1,20 +1,16 @@
-"use client"
+"use client";
 
 import { useEffect, useState, useRef } from "react";
-import { Climate_Crisis, Dela_Gothic_One } from 'next/font/google';
-import { HtmlIcon, CssIcon, JsIcon, SassIcon, PhpIcon, ReactIcon, SymfonyIcon, BootstrapIcon, MuiIcon, TailwindIcon, NextIcon } from '@/app/components/ui/icons';
+import { Climate_Crisis, Dela_Gothic_One } from "next/font/google";
+import { HtmlIcon, CssIcon, JsIcon, SassIcon, PhpIcon, ReactIcon, SymfonyIcon, BootstrapIcon, MuiIcon, TailwindIcon, NextIcon } from "@/app/components/ui/icons";
 import { gsap, Power2 } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import SplitType from "split-type";
 
-const DelaGothicOne = Dela_Gothic_One({
-    subsets: ['latin'],
-    weight: ['400'],
-    display: 'swap',
-});
+const DelaGothicOne = Dela_Gothic_One({ subsets: ["latin"], weight: ["400"], display: "swap" });
+const ClimateCrisis = Climate_Crisis({ subsets: ["latin"], display: "swap" });
 
-const ClimateCrisis = Climate_Crisis({
-    subsets: ['latin'],
-    display: 'swap',
-});
+const technos = ["html", "css", "js", "sass", "php", "react", "symfony", "bootstrap", "mui", "tailwind", "next"];
 
 interface Project {
     id: number;
@@ -30,91 +26,136 @@ interface HomeProps {
     project: Project;
 }
 
-const technos = ["html", "css", "js", "sass", "php", "react", "symfony", "bootstrap", "mui", "tailwind", "next"];
-
 export default function Tools({ project }: HomeProps) {
+    gsap.registerPlugin(ScrollTrigger);
+
     const [projet, setProjet] = useState<Project | null>(null);
     const [filteredTechnos, setFilteredTechnos] = useState<string[]>([]);
-    const parentDivRefs = useRef<HTMLDivElement[]>([]);
-    const textRefs = useRef<HTMLSpanElement[]>([]);
+    const toolDivRefs = useRef<HTMLDivElement[]>([]);
+    const h1Ref = useRef<HTMLHeadingElement>(null);
 
     useEffect(() => {
         if (project) {
             setProjet(project);
-            const filtered = project.category.filter(cat => technos.includes(cat.toLowerCase()));
+            const filtered = project.category.filter((cat) => technos.includes(cat.toLowerCase()));
             setFilteredTechnos(filtered);
         }
     }, [project]);
 
     useEffect(() => {
-        if (parentDivRefs.current.length > 0 && textRefs.current.length > 0) {
+        if (document.fonts) {
+            document.fonts.ready.then(() => {
+                const titleElement = h1Ref.current;
+                const divElements = toolDivRefs.current;
 
-            parentDivRefs.current.forEach((_, i) => {
-                const handleMouseEnter = () => {
-                    gsap.to(parentDivRefs.current[i], {
-                        width: "auto",
-                        duration: 0.5,
+                // Animation du titre avec SplitType
+                if (titleElement) {
+                    const split = new SplitType(titleElement, { types: "chars" });
+                    gsap.from(split.chars, {
+                        y: -100,
+                        duration: 0.75,
                         ease: Power2.easeOut,
+                        stagger: 0.1,
+                        scrollTrigger: {
+                            trigger: titleElement,
+                            start: "top center",
+                        },
                     });
-                    gsap.to(textRefs.current[i], {
-                        opacity: 1,
-                        display: "inline",
-                        duration: 0.5,
-                        ease: Power2.easeOut,
-                    });
-                };
+                }
 
-                const handleMouseLeave = () => {
-                    gsap.to(parentDivRefs.current[i], {
-                        width: "fit-content",
-                        duration: 0.5,
-                        ease: Power2.easeOut,
-                    });
-                    gsap.to(textRefs.current[i], {
+                // Animation des icônes
+                if (divElements.length > 0) {
+                    gsap.from(divElements, {
                         opacity: 0,
-                        display: "none",
-                        duration: 0.5,
+                        x: 50,
+                        duration: 0.75,
                         ease: Power2.easeOut,
+                        stagger: 0.25,
+                        scrollTrigger: {
+                            trigger: divElements[0],
+                            start: "top center",
+                        },
                     });
-                };
-
-                parentDivRefs.current[i].addEventListener("mouseenter", handleMouseEnter);
-                parentDivRefs.current[i].addEventListener("mouseleave", handleMouseLeave);
-
-                return () => {
-                    parentDivRefs.current[i].removeEventListener("mouseenter", handleMouseEnter);
-                    parentDivRefs.current[i].removeEventListener("mouseleave", handleMouseLeave);
-                };
-            })
+                }
+            });
         }
-    }, []);
+    }, [filteredTechnos]);
 
-    if (!projet) {
-        return <div>Loading...</div>;
-    }
+    if (!projet) return <div>Loading...</div>;
 
     return (
         <div className="pt-10 md:pt-30">
-            <h2 className={`${ClimateCrisis.className} text-xl md:text-7xl`}>Outils</h2>
+            <h2 ref={h1Ref} className={`${ClimateCrisis.className} text-xl md:text-7xl clip-path`}>
+                Outils
+            </h2>
 
             <section className={`${DelaGothicOne.className} mt-5 flex flex-wrap gap-4 md:mt-10`}>
                 {filteredTechnos.map((tech, index) => (
-                    <div ref={(el) => el && parentDivRefs.current.push(el)} key={index} className='w-max flex items-center gap-2 outline p-2 md:p-4 rounded-lg'>
-                        {tech === "html" && (<><HtmlIcon className='w-8 h-8' /> <span ref={(el) => el && textRefs.current.push(el)} className="text-html">{tech}</span></>)}
-                        {tech === "css" && (<><CssIcon className='w-8 h-8' /> <span ref={(el) => el && textRefs.current.push(el)} className="text-css">{tech}</span></>)}
-                        {tech === "js" && (<><JsIcon className='w-8 h-8' /> <span ref={(el) => el && textRefs.current.push(el)} className="text-js">{tech}</span></>)}
-                        {tech === "sass" && (<><SassIcon className='w-8 h-8' /> <span ref={(el) => el && textRefs.current.push(el)} className="text-sass">{tech}</span></>)}
-                        {tech === "php" && (<><PhpIcon className='w-8 h-8' /> <span ref={(el) => el && textRefs.current.push(el)} className="text-php">{tech}</span></>)}
-                        {tech === "react" && (<><ReactIcon className='w-8 h-8' /> <span ref={(el) => el && textRefs.current.push(el)} className="text-react">{tech}</span></>)}
-                        {tech === "symfony" && (<><SymfonyIcon className='w-8 h-8' /> <span ref={(el) => el && textRefs.current.push(el)} className="text-symfony">{tech}</span></>)}
-                        {tech === "bootstrap" && (<><BootstrapIcon className='w-8 h-8' /> <span ref={(el) => el && textRefs.current.push(el)} className="text-bootstrap">{tech}</span></>)}
-                        {tech === "mui" && (<><MuiIcon className='w-8 h-8' /> <span ref={(el) => el && textRefs.current.push(el)} className="text-materialui">{tech}</span></>)}
-                        {tech === "tailwind" && (<><TailwindIcon className='w-8 h-8' /> <span ref={(el) => el && textRefs.current.push(el)} className="text-tailwind">{tech}</span></>)}
-                        {tech === "next" && (<><NextIcon className='w-8 h-8' /> <span ref={(el) => el && textRefs.current.push(el)} className="text-symfony">{tech}</span></>)}
+                    <div
+                        ref={(el) => {
+                            if (el) toolDivRefs.current[index] = el;
+                        }}
+                        key={index}
+                        className="flex items-center gap-2 outline p-2 md:p-4 rounded-lg"
+                    >
+                        {tech === "html" && (
+                            <>
+                                <HtmlIcon className="w-8 h-8" /> <span className="text-html">{tech}</span>
+                            </>
+                        )}
+                        {tech === "css" && (
+                            <>
+                                <CssIcon className="w-8 h-8" /> <span className="text-css">{tech}</span>
+                            </>
+                        )}
+                        {tech === "js" && (
+                            <>
+                                <JsIcon className="w-8 h-8" /> <span className="text-js">{tech}</span>
+                            </>
+                        )}
+                        {tech === "sass" && (
+                            <>
+                                <SassIcon className="w-8 h-8" /> <span className="text-sass">{tech}</span>
+                            </>
+                        )}
+                        {tech === "php" && (
+                            <>
+                                <PhpIcon className="w-8 h-8" /> <span className="text-php">{tech}</span>
+                            </>
+                        )}
+                        {tech === "react" && (
+                            <>
+                                <ReactIcon className="w-8 h-8" /> <span className="text-react">{tech}</span>
+                            </>
+                        )}
+                        {tech === "symfony" && (
+                            <>
+                                <SymfonyIcon className="w-8 h-8" /> <span className="text-symfony">{tech}</span>
+                            </>
+                        )}
+                        {tech === "bootstrap" && (
+                            <>
+                                <BootstrapIcon className="w-8 h-8" /> <span className="text-bootstrap">{tech}</span>
+                            </>
+                        )}
+                        {tech === "mui" && (
+                            <>
+                                <MuiIcon className="w-8 h-8" /> <span className="text-mui">{tech}</span>
+                            </>
+                        )}
+                        {tech === "tailwind" && (
+                            <>
+                                <TailwindIcon className="w-8 h-8" /> <span className="text-tailwind">{tech}</span>
+                            </>
+                        )}
+                        {tech === "next" && (
+                            <>
+                                <NextIcon className="w-8 h-8" /> <span className="text-next">{tech}</span>
+                            </>
+                        )}
                     </div>
-                ))
-                }
-            </section >
-        </div >
+                ))}
+            </section>
+        </div>
     );
 }
