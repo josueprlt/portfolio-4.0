38a50d5c64f1ac087ebaa5b8babef404a0159a40
josueprlt <148058543+josueprlt@users.noreply.github.com>
2025-02-18 12:10:58 +0100

    add anim from tag on filterPage

--- app/components/ui/filterPage/filtered/card.tsx
+++ app/components/ui/filterPage/filtered/card.tsx
@@ -17,12 +17,33 @@ interface CardProps {
 }
 
 const Card: React.FC<CardProps> = ({ img, title, href }) => {
+    const imageRef = useRef<HTMLImageElement>(null);
+
+    const handleMouseEnter = () => {
+        gsap.to(imageRef.current, {
+            scale: 1.1,
+            duration: 0.3,
+            ease: Power2.easeOut,
+        });
+    };
+
+    const handleMouseLeave = () => {
+        gsap.to(imageRef.current, {
+            scale: 1,
+            duration: 0.3,
+            ease: Power2.easeOut,
+        });
+    };
 
     return (
-        <div className='w-full rounded-3xl border-2 border-foreground overflow-hidden card-element'>
+        <div
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
+            className='w-full rounded-3xl border-2 border-foreground overflow-hidden card-element'
+        >
             <Link href={href} className="w-full h-full">
                 <div className='h-40 rounded-3xl md:h-56'>
-                    <Image width={1000} height={1000} src={img} alt={`Image du projet ${title}`} className='w-full h-full object-cover' />
+                    <Image ref={imageRef} width={1000} height={1000} src={img} alt={`Image du projet ${title}`} className='w-full h-full object-cover' />
                 </div>
                 <p className={`${DelaGothicOne.className} h-auto p-4 text-xl md:p-6 md:text-2xl`}>{title}</p>
             </Link>
--- app/components/ui/filterPage/tags/tag.tsx
+++ app/components/ui/filterPage/tags/tag.tsx
@@ -1,5 +1,4 @@
-import { useEffect, useRef } from 'react';
-import Link from 'next/link';
+import { useRef } from 'react';
 import { gsap, Power2 } from 'gsap';
 import { CloseIcon } from '@/app/components/ui/icons';
 
@@ -9,11 +8,52 @@ interface TagProps {
 }
 
 const Tag: React.FC<TagProps> = ({ name, onClick }) => {
+    const divRef = useRef<HTMLDivElement>(null);
+    const textRef = useRef<HTMLParagraphElement>(null);
+
+    const handleMouseEnter = () => {
+        gsap.to(divRef.current, {
+            scale: 10,
+            x: -10,
+            transformOrigin: "left",
+            duration: 0.3,
+            ease: Power2.easeOut,
+        });
+        gsap.to(textRef.current, {
+            color: '#FEEFDD',
+            duration: 0.3,
+            ease: Power2.easeOut,
+        });
+    };
+
+    const handleMouseLeave = () => {
+        gsap.to(divRef.current, {
+            scale: 1,
+            x: 0,
+            duration: 0.3,
+            ease: Power2.easeOut,
+        });
+        gsap.to(textRef.current, {
+            color: '#262330',
+            duration: 0.3,
+            ease: Power2.easeOut,
+        });
+    };
 
     return (
-        <button className='flex items-center whitespace-nowrap p-[5px] pr-4 gap-2 border-2 border-foreground rounded-full' onClick={onClick}>
-            <CloseIcon className='md:w-8 md:h-8' />
-            <p className='font-sans font-bold md:text-xl'>{name}</p>
+        <button
+            className='relative flex items-center whitespace-nowrap p-[5px] pr-4 gap-2 border-2 border-foreground rounded-full z-10 overflow-hidden'
+            onClick={onClick}
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
+        >
+            <div className='relative w-[25px] h-[25px] bg-secondary rounded-full z-10'>
+                <CloseIcon className='md:w-[25px] md:h-[25px] z-10' />
+            </div>
+            <div className='relative z-10'>
+                <p ref={textRef} className='font-sans font-bold md:text-xl'>{name}</p>
+            </div>
+            <div ref={divRef} className='absolute w-[25px] h-[25px] bg-secondary rounded-full z-0'></div>
         </button>
     );
 };
