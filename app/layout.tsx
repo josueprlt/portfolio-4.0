aa2a4557c7f5596d2d095c10501305b22a9473cf
josueprlt <148058543+josueprlt@users.noreply.github.com>
2025-03-17 22:52:24 +0100

    Update : Rendre la navbar fixe en top de l'écran

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,13 @@
 "use client"
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import Head from "next/head";
 import Footer from "@/app/components/ui/footer/page";
+import Navbar from "@/app/components/ui/navBar/page";
 import localFont from "next/font/local";
 import "./globals.css";
 import LoadingScreen from "@/app/components/ui/LoadingScreen/page";
 import { metadata } from "./metada";
+import { gsap } from "gsap";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -24,6 +26,45 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   const [loading, setLoading] = useState(true);
+  const [isScrolled, setIsScrolled] = useState(false);
+  const [scrollWidth, setScrollWidth] = useState(0);
+  const sectionRef = useRef<HTMLElement>(null);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollTop = window.scrollY;
+      const docHeight = document.documentElement.scrollHeight - window.innerHeight;
+      const scrollPercent = (scrollTop / docHeight) * 100;
+      setIsScrolled(scrollTop > 75);
+      setScrollWidth(scrollPercent);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  useEffect(() => {
+    gsap.to(sectionRef.current, {
+      y: -68,
+      duration: 0.5,
+      ease: "power2.out",
+    });
+    if (isScrolled) {
+      gsap.to(sectionRef.current, {
+        y: 0,
+        duration: 0.5,
+        ease: "power2.out",
+      });
+    } else {
+      gsap.to(sectionRef.current, {
+        y: -68,
+        duration: 0.5,
+        ease: "power2.out",
+      });
+    }
+  }, [isScrolled]);
 
   return (
     <html lang="en">
@@ -31,11 +72,27 @@ export default function RootLayout({
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} relative antialiased`}
       >
         {loading && <LoadingScreen onComplete={() => setLoading(false)} />}
-        {!loading && children}
-        {!loading && <Footer />}
+        {!loading && (
+          <>
+            <section
+              ref={sectionRef}
+              className={`fixed top-0 left-0 right-0 z-50 bg-background`}
+            >
+              <span
+                style={{ width: `${scrollWidth}%` }}
+                className="block h-1 bg-gradient-to-r from-primary to-secondary"
+              ></span>
+              <div className="px-4 py-1 md:px-8 md:py-2">
+                <Navbar />
+              </div>
+            </section>
+            {children}
+            <Footer />
+          </>
+        )}
       </body>
     </html>
   );
